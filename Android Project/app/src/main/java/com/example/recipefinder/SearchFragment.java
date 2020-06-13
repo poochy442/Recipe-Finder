21245cd62d5e62e8f1905f3a4f614b1479dd67c6
Kenneth Jensen <42805394+poochy442@users.noreply.github.com>
2020-06-13 15:56:31 +0200

    Save for good measure

--- Android Project/app/src/main/java/com/example/recipefinder/SearchFragment.java
+++ Android Project/app/src/main/java/com/example/recipefinder/SearchFragment.java
@@ -31,7 +31,7 @@ public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.search_result, container, false);
+        View rootView = inflater.inflate(R.layout.search_fragment, container, false);
         populateView(rootView);
         return rootView;
     }
